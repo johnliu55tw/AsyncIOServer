32b3ee4eb531a10899e1adc1b9f908972eaf0db5
John Liu <johnliu55tw@gmail.com>
2017-03-10 12:02:24 +0800

    Add argument parser

--- SimpleDashboardServer.py
+++ SimpleDashboardServer.py
@@ -1,18 +1,15 @@
 #!/usr/bin/env python3
 
+import argparse
 import selectors
 import socket
 import ssl
 
 
-CERT_FILE = "/home/john/SslCert/server.crt"
-KEY_FILE = "/home/john/SslCert/server.key"
-
-
-def createServerSocket(addressTuple, listenNumber=100):
+def createServerSocket(host, port, listenNumber=100):
     serverSock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     serverSock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    serverSock.bind(addressTuple)
+    serverSock.bind((host, port))
     serverSock.listen(listenNumber)
     serverSock.setblocking(False)
     return serverSock
@@ -36,35 +33,47 @@ def clientRead(selectorObj, clientSock, mask):
 
 def acceptClientSocket(selectorObj, serverSock, mask):
     clientSock, addr = serverSock.accept()  # Should be ready
-    print('Accepted regular connection from: {}'.format(addr))
-    clientSock.setblocking(False)
-    selectorObj.register(clientSock, selectors.EVENT_READ, clientRead)
-
-
-def acceptSslClientSocket(selectorObj, serverSock, mask):
-    # Create SSL context
-    sslCtx = ssl.create_default_context(ssl.Purpose.CLIENT_AUTH)
-    sslCtx.load_cert_chain(certfile=CERT_FILE, keyfile=KEY_FILE)
-
-    clientSock, addr = serverSock.accept()  # Should be ready
-    print('Accepted SSL connection from: {}'.format(addr))
-    clientSock = sslCtx.wrap_socket(clientSock, server_side=True)
+    print('Port {}: Accepted regular connection from: {}'.format(
+        serverSock.getsockname()[1], addr))
     clientSock.setblocking(False)
     selectorObj.register(clientSock, selectors.EVENT_READ, clientRead)
 
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--host', type=str, default="0.0.0.0",
+                        help="The host to listen on. Default to 0.0.0.0")
+    parser.add_argument('--port', type=int, default=9001,
+                        help="The port number. Default to 9001")
+    parser.add_argument('--listen', type=int, default=10,
+                        help="The listen number. Default to 10.")
+    parser.add_argument('--ssl', action='store_true', default=False,
+                        help="Enable SSL/TLS. Disabled by default.")
+    parser.add_argument('--cert', type=str,
+                        help="The path of the certificate *.crt file.")
+    parser.add_argument('--key', type=str,
+                        help="The path of the private key *.key file.")
+    args = parser.parse_args()
+
     sel = selectors.DefaultSelector()
-    # For regular TCP socket
-    sel.register(createServerSocket(('0.0.0.0', 9001)),
+    # Register server socket
+    if args.ssl is True:
+        print("Waiting for SSL connection on {}:{}".format(
+            args.host, args.port))
+        serverSock = ssl.wrap_socket(
+                sock=createServerSocket(args.host, args.port, args.listen),
+                keyfile=args.key,
+                certfile=args.cert,
+                server_side=True)
+    else:
+        print("Waiting for unencrypted TCP connection on {}:{}".format(
+            args.host, args.port))
+        serverSock = createServerSocket(args.host, args.port, args.listen)
+
+    sel.register(serverSock,
                  selectors.EVENT_READ,
                  acceptClientSocket)
-    # For SSL TCP socket
-    sel.register(createServerSocket(('0.0.0.0', 9002)),
-                 selectors.EVENT_READ,
-                 acceptSslClientSocket)
 
-    print("Entering event loop. Wait for connection.")
     while True:
         events = sel.select()
         for key, mask in events:
